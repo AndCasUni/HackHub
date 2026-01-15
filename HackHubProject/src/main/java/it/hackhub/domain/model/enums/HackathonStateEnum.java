9f5f9022695f21ed036b486028ec61e553e126a2
AndCasUni <andrea03.castelli@studenti.unicam.it>
2026-01-15 12:28:23 +0100

    Delete HackHubProject/src/main/java/it/hackhub/domain directory

--- HackHubProject/src/main/java/it/hackhub/domain/model/enums/HackathonStateEnum.java
+++
@@ -1,8 +0,0 @@
-package it.hackhub.domain.model.enums;
-
-public enum HackathonStateEnum {
-    REGISTRATION,  // Iscrizione aperte
-    RUNNING,       // In corso
-    EVALUATION,    // Valutazione sottomissioni
-    CLOSED;        // Concluso, vincitore proclamato
-}
